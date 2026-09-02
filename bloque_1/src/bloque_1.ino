47274922627301449d63db031f6c04ba78147008
JuanDanielx <juan.mendezm.est@uets.edu.ec>
2026-09-02 12:54:32 -0500

    feat(diagnostica): bloque1: inicializacion de variables pinLedRojo pinLedVerde como salida y un pin de entrada PinPulsante configuracion de pulsante que mediante su lectura enciende y apaga el led rojo y verde. bloque2: Se realizo las correciones del codigo añadiendo el analog.read y ocupando la regla tres para que el potenciometro de un resultado similar al voltaje que da

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -1,25 +1,27 @@
 const int pinLedRojo = 23;
 const int pinLedVerde = 22;
-const int pinPulsador = 4;
+const int pinPulsador = 21;
 
 void setup() {
   Serial.begin(115200);
-  // TODO: Configurar los pines de los LEDs como OUTPUT
-  
-  // TODO: Configurar el pin del pulsador como INPUT
+pinMode(pinLedRojo, OUTPUT);
+pinMode(pinLedVerde, OUTPUT);
+pinMode(pinPulsador, INPUT);
   
 }
 
 void loop() {
-  // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador = 0; // Cambia esto
+  digitalRead(pinPulsador);
+  int estadoPulsador = digitalRead(pinPulsador); 
   
   if (estadoPulsador == HIGH) {
-    // TODO: Encender LED Rojo y apagar LED Verde
+    digitalWrite(pinLedRojo, HIGH);
+    digitalWrite(pinLedVerde, LOW);
     
     Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
   } else {
-    // TODO: Apagar LED Rojo y encender LED Verde
+    digitalWrite(pinLedRojo, LOW );
+    digitalWrite(pinLedVerde, HIGH );
     
     Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
   }
