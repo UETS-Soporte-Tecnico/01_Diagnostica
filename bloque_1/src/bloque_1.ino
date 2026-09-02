c4174bb40cc5ea9f77cc78522431a8d50a7216a7
GalanJuan <juan.galanr.est@uets.edu.ec>
2026-09-02 12:51:06 -0500

    feat: (diagnostica) bloque 1

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -3,26 +3,36 @@ const int pinLedVerde = 22;
 const int pinPulsador = 4;
 
 void setup() {
-  Serial.begin(115200);
-  // TODO: Configurar los pines de los LEDs como OUTPUT
-  
-  // TODO: Configurar el pin del pulsador como INPUT
-  
+Serial.begin(115200);
+
+
+pinMode (pinLedRojo, OUTPUT);
+pinMode (pinLedVerde, OUTPUT);
+
+pinMode (pinPulsador, INPUT);
 }
 
+
 void loop() {
-  // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador = 0; // Cambia esto
-  
-  if (estadoPulsador == HIGH) {
-    // TODO: Encender LED Rojo y apagar LED Verde
+
+int estadoPulsador = digitalRead(pinPulsador);
+
+if (estadoPulsador == HIGH) {
+
+digitalWrite(pinLedRojo, 1);
+digitalWrite(pinLedVerde, 0);
+Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
+} else {
+
+
+
     
-    Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
-  } else {
-    // TODO: Apagar LED Rojo y encender LED Verde
-    
-    Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
-  }
-  
-  delay(100);
+digitalWrite(pinLedRojo, 0);
+digitalWrite(pinLedVerde, 1);
+Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
+}
+
+
+
+delay(100);
 }
--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -1,21 +1,31 @@
 const int pinPot = 34;
 
 void setup() {
-  Serial.begin(115200);
+Serial.begin(115200);
 }
 
+
 void loop() {
-  // TODO: Leer el valor del potenciómetro usando analogRead
-  int lecturaADC = 0; // Cambia esto
-  
-  // TODO: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
-  float voltaje = 0.0; // Cambia esto aplicando la regla de tres
-  
-  Serial.print("ADC: ");
-  Serial.print(lecturaADC);
-  Serial.print(" | Voltaje: ");
-  Serial.print(voltaje, 2);
-  Serial.println("V");
-  
-  delay(500);
+
+
+int lecturaADC = analogRead(pinPot);; 
+
+
+
+
+float voltaje = (lecturaADC * 3.3)/ 4095.0;; 
+
+
+Serial.print("ADC: ");
+Serial.print(lecturaADC);
+Serial.print(" | Voltaje: ");
+Serial.print(voltaje, 2);
+Serial.println("V");
+
+
+
+
+
+
+delay(500);
 }
