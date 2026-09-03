697586f1a59a4ae259465068266569e5cd205da8
Andrel0ja <andre.lojab.est@uets.edu.ec>
2026-09-02 22:32:42 -0500

    feat(diagnostica): primer ejercicio configuré los LEDs como salidas y el pulsador como entrada. Después usé digitalRead para saber el estado del botón y digitalWrite para controlar los LEDs; segundo ejercicio usé analogRead para leer el potenciómetro. Como el ESP32 tiene una lectura de 0 a 4095, convertí ese valor a un voltaje de 0 a 3.3 V usando una regla de tres.

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -1,28 +1,35 @@
-const int pinLedRojo = 23;
-const int pinLedVerde = 22;
-const int pinPulsador = 4;
-
-void setup() {
-  Serial.begin(115200);
-  // TODO: Configurar los pines de los LEDs como OUTPUT
-  
-  // TODO: Configurar el pin del pulsador como INPUT
-  
-}
-
-void loop() {
-  // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador = 0; // Cambia esto
-  
-  if (estadoPulsador == HIGH) {
-    // TODO: Encender LED Rojo y apagar LED Verde
-    
-    Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
-  } else {
-    // TODO: Apagar LED Rojo y encender LED Verde
-    
-    Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
-  }
-  
-  delay(100);
+const int pinLedRojo = 23; 
+const int pinLedVerde = 22; 
+const int pinPulsador = 4; 
+ 
+void setup() { 
+  Serial.begin(115200); 
+  // TODO: Configurar los pines de los LEDs como OUTPUT 
+  pinMode(pinLedRojo, OUTPUT);
+  pinMode(pinLedVerde, OUTPUT);
+ 
+  // TODO: Configurar el pin del pulsador como INPUT 
+  pinMode(pinPulsador, INPUT);
+ 
+} 
+ 
+void loop() { 
+  // TODO: Leer el estado del pulsador usando digitalRead 
+  int estadoPulsador = digitalRead(pinPulsador);
+   
+  if (estadoPulsador == HIGH) { 
+    // TODO: Encender LED Rojo y apagar LED Verde 
+    digitalWrite(pinLedRojo, HIGH);
+    digitalWrite(pinLedVerde, LOW);
+     
+    Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO"); 
+  } else { 
+    // TODO: Apagar LED Rojo y encender LED Verde 
+    digitalWrite(pinLedRojo, LOW);
+    digitalWrite(pinLedVerde, HIGH);
+     
+    Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO"); 
+  } 
+   
+  delay(100); 
 }
