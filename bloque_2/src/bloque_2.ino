afce0ef143c19598060d2895e2d172a5022668ca
Alesita-p <alejandrapsos24@gmail.com>
2026-09-08 18:42:04 -0500

    feat(bloque2): ya funciona el potenciometro y se quitaron los comentarios

--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -5,11 +5,9 @@ void setup() {
 }
 
 void loop() {
-  // TODO: Leer el valor del potenciómetro usando analogRead
-  int lecturaADC = 0; // Cambia esto
+  int lecturaADC = analogRead(pinPot);
   
-  // TODO: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
-  float voltaje = 0.0; // Cambia esto aplicando la regla de tres
+  float voltaje = (lecturaADC * 3.3) / 4095.0;
   
   Serial.print("ADC: ");
   Serial.print(lecturaADC);
