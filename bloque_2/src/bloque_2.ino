4e56a6e37084d2227e68376d8904d53c5ad91d41
millimpact-netizen <millimpact@gmail.com>
2026-09-07 22:25:50 -0500

    feat(bloque_2):Resolví el ejercicio número 2.

--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -5,17 +5,21 @@ void setup() {
 }
 
 void loop() {
-  // TODO: Leer el valor del potenciómetro usando analogRead
-  int lecturaADC = 0; // Cambia esto
-  
-  // TODO: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
-  float voltaje = 0.0; // Cambia esto aplicando la regla de tres
-  
+
+  // Leer el valor del potenciómetro
+  int lecturaADC = analogRead(pinPot);
+
+  // Convertir ADC a voltaje
+  float voltaje = lecturaADC * (3.3 / 4095.0);
+
   Serial.print("ADC: ");
   Serial.print(lecturaADC);
+
   Serial.print(" | Voltaje: ");
+
   Serial.print(voltaje, 2);
+
   Serial.println("V");
-  
+
   delay(500);
 }
