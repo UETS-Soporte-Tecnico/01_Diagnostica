7416fccd8d46ea49d126065fcd8a40a14194c0f3
Danyx <marcop.morocho17@gmail.com>
2026-09-07 21:33:23 -0500

    feat(Bloque_a):Completar el codigo dado

--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -5,11 +5,8 @@ void setup() {
 }
 
 void loop() {
-  // TODO: Leer el valor del potenciómetro usando analogRead
-  int lecturaADC = 0; // Cambia esto
-  
-  // TODO: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
-  float voltaje = 0.0; // Cambia esto aplicando la regla de tres
+  int lecturaADC = analogRead(pinPot);
+  float voltaje = (lecturaADC * 3.3) / 4095.0;
   
   Serial.print("ADC: ");
   Serial.print(lecturaADC);
