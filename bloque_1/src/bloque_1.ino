4bf070b7c0ed320c00a7a29442a66be0ded515c3
millimpact-netizen <millimpact@gmail.com>
2026-09-07 21:55:30 -0500

    feat)bloque_1): Primer ejercicio resuelto

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -4,25 +4,32 @@ const int pinPulsador = 4;
 
 void setup() {
   Serial.begin(115200);
-  // TODO: Configurar los pines de los LEDs como OUTPUT
-  
-  // TODO: Configurar el pin del pulsador como INPUT
-  
+
+  // Configurar los pines de los LEDs como OUTPUT
+  pinMode(pinLedRojo, OUTPUT);
+  pinMode(pinLedVerde, OUTPUT);
+
+  // Configurar el pin del pulsador como INPUT
+  pinMode(pinPulsador, INPUT);
 }
 
 void loop() {
-  // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador = 0; // Cambia esto
-  
+  // Leer el estado del pulsador
+  int estadoPulsador = digitalRead(pinPulsador);
+
   if (estadoPulsador == HIGH) {
-    // TODO: Encender LED Rojo y apagar LED Verde
-    
+    // Encender LED Rojo y apagar LED Verde
+    digitalWrite(pinLedRojo, HIGH);
+    digitalWrite(pinLedVerde, LOW);
+
     Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
   } else {
-    // TODO: Apagar LED Rojo y encender LED Verde
-    
+    // Apagar LED Rojo y encender LED Verde
+    digitalWrite(pinLedRojo, LOW);
+    digitalWrite(pinLedVerde, HIGH);
+
     Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
   }
-  
+
   delay(100);
 }
